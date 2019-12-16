6b99a5bc2427d34d4a47f0bef66b29723dbbf781
Johee Chung <johee.chung@nike.com>
2019-12-15 18:21:24 -0800

    Set up progress get

--- functions/index.js
+++ functions/index.js
@@ -3,57 +3,65 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
 admin.initializeApp(functions.config().firebase);
-
-exports.date = functions.https.onRequest((req, res) => {
-  res.status(200).send('JOHEE');
-});
-
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  const original = req.query.text;
-  const writeResult = await admin.firestore().collection('messages').add({ original: original });
-  res.json({ result: `Message with ID: ${writeResult.id} added.` });
-});
-
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-  .onCreate((snap, context) => {
-    const original = snap.data().original;
-    console.log('Uppercasing', context.params.documentID, original);
-    const uppercase = original.toUpperCase();
-    return snap.ref.set({uppercase}, {merge: true});
-  });
-
 let db = admin.firestore();
-let docRef = db.collection('users').doc('UH1tRz041Tap0Ptm6ZJV0QRgxgy1');
 
-let setProgress = docRef.set({
+const requirements = {
   20200229: {
     strategyHour: [
-      { signin: true, date: new Date('12/14/2019') },
-      { signin: true, date: new Date('12/21/2019') },
+      { signin: false, date: null },
+      { signin: false, date: null },
       { signin: false, date: null },
     ],
     practice: [
-      { signin: true, date: new Date('12/19/2019') },
-      { signin: true, date: new Date('12/16/2019') },
+      { signin: false, date: null },
+      { signin: false, date: null },
       { signin: false, date: null },
     ],
     scrimmage: [
-      { signin: true, date: new Date('12/14/2019') },
-      { signin: true, date: new Date('12/21/2019') },
+      { signin: false, date: null },
+      { signin: false, date: null },
       { signin: false, date: null },
       { signin: false, date: null },
     ],
-    volunteer1: { vologistic: true, hours: 3 },
+    volunteer1: { vologistic: false, hours: 0 },
     volunteer2: { vologistic: false, hours: 0 },
   }
-});
+};
 
-db.collection('users').get()
-  .then((snapshot) => {
-    snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
-    });
-  })
-  .catch((err) => {
-    console.log('Error getting documents', err);
-  });
+const getEligibility = async (collRef, uid) => (
+  await collRef.get()
+    .then(snapshot => {
+      let docs = []
+      snapshot.forEach(doc => {
+        if (doc.id === uid) {
+          docs.push(doc);
+        }
+      })
+      return docs;
+    })
+    .then(entries => {
+      if(entries.length === 0) {
+        collRef.doc(uid).set(requirements);
+        return requirements;
+      }
+      const entry = {
+        ...requirements,
+        ...entries[0].data()
+      };
+      collRef.doc(uid).set(entry);
+      return entry;
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    })
+);
+
+exports.eligibility = functions.https.onRequest(async (req, res) => {
+  try {
+    const collRef = await db.collection('users');
+    const userData = await getEligibility(collRef, req.query.uid);
+    res.status(200).send(JSON.stringify(userData));
+  } catch (err) {
+    console.log({ err });
+  }
+});
